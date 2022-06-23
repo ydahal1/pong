dfd5d0ba6eecfb1e10c449bec3c3732e853f0a36
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2022-06-23 13:54:53 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `pong`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
